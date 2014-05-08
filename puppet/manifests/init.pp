15f6daf018331fc3e832c951d11d395da49166cd
Tegan Snyder <tsnyder@tegdesign.com>
2014-05-08 14:21:51 -0500

    Update init.pp

--- puppet/manifests/init.pp
+++ puppet/manifests/init.pp
@@ -46,3 +46,50 @@ include php
 include mailcatcher
 include git
 include tools
+
+#################################################
+# Ruby
+#################################################
+
+$as_vagrant   = 'sudo -u vagrant -H bash -l -c'
+$home         = '/home/vagrant'
+
+class rvm {
+
+  exec { 'install_rvm':
+    command => "${as_vagrant} 'curl -L https://get.rvm.io | bash -s stable'",
+    creates => "${home}/.rvm/bin/rvm",
+    require => Package['curl']
+  }
+
+  exec { 'install_ruby':
+    # We run the rvm executable directly because the shell function assumes an
+    # interactive environment, in particular to display messages or ask questions.
+    # The rvm executable is more suitable for automated installs.
+    #
+    # Thanks to @mpapis for this tip.
+    command => "${as_vagrant} '${home}/.rvm/bin/rvm install 2.0.0 --latest-binary --autolibs=enabled && rvm --fuzzy alias create default 2.0.0'",
+    creates => "${home}/.rvm/bin/ruby",
+    require => Exec['install_rvm']
+  }
+
+  exec { "${as_vagrant} 'gem install bundler --no-rdoc --no-ri'":
+    creates => "${home}/.rvm/bin/bundle",
+    require => Exec['install_ruby']
+  }
+
+  exec { "${as_vagrant} 'gem install --version 2.15.4 capistrano'":
+    require => Exec['install_ruby']
+  }
+
+  exec { "${as_vagrant} 'gem install --version 0.0.6 magentify'":
+    require => Exec['install_ruby']
+  }
+  
+  exec { "${as_vagrant} 'gem install compass'":
+    require => Exec['install_ruby']
+  }
+
+}
+
+include rvm
